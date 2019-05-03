ef4ac23e9a6248a55ac9cd551e51e223d224a2da
vysoupa1 <pavelvysoudil97@gmail.com>
2019-05-03 14:01:29 +0200

    Implementace Areas zatim nefunkcni

---
+++ Alkoshop/Areas/Customer/CustomerAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Alkoshop.Areas.Customer
+{
+    public class CustomerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Customer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Customer_default",
+                "Customer/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] {"Alkoshop.Areas.Customer.Controllers"}
+            );
+        }
+    }
+}
