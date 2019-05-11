89c235a1b0153167625c9cb1484cd3b5198d9b0d
Jiří Dřímal <georgedrimal@seznam.cz>
2019-05-11 16:51:46 +0200

    Country do Productu

--- Alkoshop/Controllers/HomeController.cs
+++ Alkoshop/Controllers/HomeController.cs
@@ -1,15 +1,8 @@
 ﻿using Alkoshop.Database;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Oracle.DataAccess.Client;
 using Alkoshop.Models;
-using System.Drawing;
-using Alkoshop.Class;
-using System.Drawing.Imaging;
-using System.IO;
 
 namespace Alkoshop.Controllers
 {
